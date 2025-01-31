3db335bd4fe70a5931adf440f334edf7f8dedba1
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-31 14:22:10 +0700

    Fix hover event on grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 let container = document.querySelector(".container");
 
-const div = document.createElement("div");
-div.classList.add("divGrid");
-console.log(div);
-
 const gridSize_x = 16;
 const gridSize_y = 16;
 const gridSize = gridSize_x * gridSize_y;
-for (let i = 1; i <= gridSize; i++) {
-  container.innerHTML += div.outerHTML;
+
+for (let i = 0; i < gridSize; i++) {
+  const div = document.createElement("div");
+  div.classList.add("divGrid");
+  container.appendChild(div);
+  div.addEventListener("mouseover", function () {
+    div.style.backgroundColor = "black";
+  });
 }
