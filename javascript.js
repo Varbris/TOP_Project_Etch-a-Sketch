08a0ea9531cffd17a107a741aedf36f8d199ee58
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-31 15:39:42 +0700

    Add event to sizeButton

--- javascript.js
+++ javascript.js
@@ -1,19 +1,29 @@
-let container = document.querySelector(".container");
+function generateGrid(userInput = 16) {
+  let gridSize_x = userInput;
+  let gridSize_y = userInput;
+  let gridSize = gridSize_x * gridSize_y;
 
-const gridSize_x = 16;
-const gridSize_y = 16;
-const gridSize = gridSize_x * gridSize_y;
-
-for (let i = 0; i < gridSize; i++) {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  const div = document.createElement("div");
-  div.classList.add("divGrid");
-  div.classList.add("flex-grow");
-  container.appendChild(div);
-  div.addEventListener("mouseover", function (e) {
-    e.target.style.backgroundColor = "#" + randomColor;
-    setTimeout(function () {
-      e.target.style.backgroundColor = "";
-    }, 300);
-  });
+  for (let i = 0; i < gridSize; i++) {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const div = document.createElement("div");
+    div.classList.add("divGrid");
+    div.classList.add("flex-grow");
+    container.appendChild(div);
+    div.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "#" + randomColor;
+      setTimeout(function () {
+        e.target.style.backgroundColor = "";
+      }, 300);
+    });
+  }
 }
+
+const container = document.querySelector(".container");
+const sizeButton = document.querySelector("#sizeButton");
+generateGrid();
+
+sizeButton.addEventListener("click", function () {
+  let userInput = prompt("What size do you want ? (pick a number)");
+  container.innerHTML = "";
+  generateGrid(userInput);
+});
