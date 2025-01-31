fec7bf5cd8e9159fe3aa325e0f72231c150543f7
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-31 15:00:20 +0700

    Add size button

--- javascript.js
+++ javascript.js
@@ -5,10 +5,15 @@ const gridSize_y = 16;
 const gridSize = gridSize_x * gridSize_y;
 
 for (let i = 0; i < gridSize; i++) {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   const div = document.createElement("div");
   div.classList.add("divGrid");
+  div.classList.add("flex-grow");
   container.appendChild(div);
-  div.addEventListener("mouseover", function () {
-    div.style.backgroundColor = "black";
+  div.addEventListener("mouseover", function (e) {
+    e.target.style.backgroundColor = "#" + randomColor;
+    setTimeout(function () {
+      e.target.style.backgroundColor = "";
+    }, 300);
   });
 }
